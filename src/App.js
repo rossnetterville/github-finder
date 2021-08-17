486a5690ef44432b5b6e482637d47d658c833146
rossnetterville <ross_netterville@hotmail.com>
2021-08-17 14:36:04 +0200

    added userItem component and state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, {Component} from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar'
+import UserItem from './components/users/UserItem';
 
 class App extends Component {
 
   render () {
 
      return (
-    <div className="App">
-      <Navbar />
-    </div>
+
+      <div className="App">
+        <Navbar />
+        <UserItem />
+      </div>
+
   );
   }
  
