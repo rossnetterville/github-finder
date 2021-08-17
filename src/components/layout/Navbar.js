f8b8a88104144255f7bc9df6aaf7356f0d3294a6
rossnetterville <ross_netterville@hotmail.com>
2021-08-17 15:04:03 +0200

    refactored to use functional components

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,27 +1,27 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 
 
-export class Navbar extends Component {
+const Navbar =  ({icon, title}) =>  {
 
-    static defaultProps = {
-        title: 'GitHub Finder',
-        icon: 'fab fa-github'
-    }
-
-    static propTypes = {
-        title: PropTypes.string.isRequired,
-        icon: PropTypes.string.isRequired
-    }
-    render() {
         return (
             <nav className="navbar bg-primary">
                 <h1>
-                <i class={this.props.icon} /> {this.props.title}
+                <i className={icon} /> {title}
                 </h1>
             </nav>
         )
-    }
+}
+
+
+Navbar.defaultProps = {
+    title: 'GitHub Finder',
+    icon: 'fab fa-github'
+}
+
+Navbar.propTypes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired
 }
 
 export default Navbar
