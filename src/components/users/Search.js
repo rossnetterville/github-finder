c5fc4b123ecacce61c12cc1e5d1a4451791cc023
rossnetterville <ross_netterville@hotmail.com>
2021-08-18 14:58:06 +0200

    Added Search Query

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 export class Search extends Component {
     state = {
         text: ''
+    }
+
+    static propTypes ={
+        searchUsers: PropTypes.func.isRequired,
     }
 
     onChange = (e) => {
@@ -11,22 +16,26 @@ export class Search extends Component {
 
     onSubmit = (e) => {
          e.preventDefault()
-         console.log(this.state.text)
+         this.props.searchUsers(this.state.text)
+         this.setState({text: ''})
      }
 
     render() {
         return (
             <div>
-                <form onSubmit={this.onSubmit} className="form">
-                    <input 
-                    type="text" 
-                    name="text" 
-                    placeholder="Search Users..." 
-                    value={this.state.text}
-                    onChange={this.onChange}
-                    />
-                    <input type="submit" value="Search" className="btn btn-dark btn-block" />
-                </form>
+                 <form className='form' onSubmit={this.onSubmit}>
+          <input
+            type='text'
+            name='text'
+            placeholder='Search Users...'
+            onChange={this.onChange}
+          />
+          <input
+            type='submit'
+            value='Search'
+            className='btn btn-dark btn-block'
+          />
+        </form>
             </div>
         )
     }
