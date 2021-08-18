60d9f7ab6d69a715275208048ec9825f8bed2353
rossnetterville <ross_netterville@hotmail.com>
2021-08-18 14:12:47 +0200

    Created Search Component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar'
 import Users from './components/users/Users';
+import Search from './components/users/Search'
 import axios from 'axios';
 
 class App extends Component {
@@ -12,10 +13,9 @@ class App extends Component {
   }
 
   async componentDidMount() {
-
     this.setState({loading: true})
 
-    const res = await axios.get('https://api.github.com/users')
+    const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
     
     
     this.setState({users: res.data, loading: false})
@@ -26,6 +26,7 @@ class App extends Component {
       <div className="App">
         <Navbar />
         <div className="container">
+          <Search />
           <Users loading={this.state.loading} users={this.state.users}/>
         </div>
       </div>
