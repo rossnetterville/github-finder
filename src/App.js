b747b5cf27007cf87e353c41726ef95a222e50d2
rossnetterville <ross_netterville@hotmail.com>
2021-08-23 13:49:43 +0200

    added alert and alert timeout

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import Navbar from './components/layout/Navbar'
 import Users from './components/users/Users';
 import Search from './components/users/Search'
+import Alert from './components/layout/Alert'
 import axios from 'axios';
 
 class App extends Component {
 
   state = {
     users: [],
-    loading: false
+    loading: false,
+    alert: null
   }
 
 
@@ -29,6 +31,15 @@ class App extends Component {
     this.setState({users: [], loading:false, text:''})
   }
 
+  // set Alert
+
+  setAlert = (msg, type) => {
+    this.setState({alert: {msg: msg, type: type}})
+
+    setTimeout( () => this.setState({alert:null}), 3000)
+  }
+
+
   render () {
 
     const {users, loading} = this.state
@@ -38,7 +49,12 @@ class App extends Component {
       <div className="App">
         <Navbar />
         <div className="container">
-          <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={users.length > 0 ? true : false}/>
+          <Alert alert={this.state.alert}/>
+          <Search 
+            searchUsers={this.searchUsers} 
+            clearUsers={this.clearUsers} 
+            showClear={users.length > 0 ? true : false}
+            setAlert={this.setAlert}/>
           <Users loading={loading} users={users}/>
         </div>
       </div>
