ef5e591698414c65a6c65fc9cec17513f28657d2
rossnetterville <ross_netterville@hotmail.com>
2021-09-03 09:19:17 +0200

    refactored User component to be functional component and useState hook

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,24 +1,20 @@
-import React, { Component, Fragment } from 'react'
+import React, { useEffect, Fragment } from 'react'
 import { Spinner } from '../layout/Spinner'
 import Repos from '../repos/Repos'
 import PropTypes from 'prop-types'
 import {Link} from 'react-router-dom'
 
 
-export class User extends Component {
-    componentDidMount() {
-        this.props.getUser(this.props.match.params.login)
-        this.props.getUserRepos(this.props.match.params.login)
-    }
+const User = ({user, loading, getUser, getUserRepos, repos, match}) => {
 
-    static propTypes = {
-        loading: PropTypes.bool,
-        repos: PropTypes.array.isRequired,
-        user: PropTypes.object.isRequired,
-        getUser: PropTypes.func.isRequired,
-        getUserRepos: PropTypes.func.isRequired,
-    }
-    render() {
+    useEffect(() =>{
+        getUser(match.params.login)
+        getUserRepos(match.params.login)
+        // eslint-disable-next-line
+    }, [])
+    
+
+
         const {
             name, 
             avatar_url, 
@@ -32,9 +28,9 @@ export class User extends Component {
             public_repos, 
             public_gists,
             company, 
-            hireable} = this.props.user
+            hireable} = user
 
-            const {loading, repos} = this.props
+
         if(loading) return <Spinner />
         return (
             <Fragment>
@@ -94,7 +90,14 @@ export class User extends Component {
                 <Repos repos={repos} />
             </Fragment>
         )
-    }
+}
+
+User.propTypes = {
+    loading: PropTypes.bool,
+    repos: PropTypes.array.isRequired,
+    user: PropTypes.object.isRequired,
+    getUser: PropTypes.func.isRequired,
+    getUserRepos: PropTypes.func.isRequired,
 }
 
 export default User
